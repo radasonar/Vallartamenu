e35bfeee8d483286d152bc09ae93b8c4b347a1f7
radasonar <rada-99@hotmail.com>
2014-03-07 16:36:57 -0600

    mas avances
    contenido detalles

--- php/admin_contenido_detalles.php
+++ php/admin_contenido_detalles.php
@@ -1,13 +1,89 @@
-<h1 class="seccion">Detalles</h1>
-<hr>
-<div class="agrupar">
-	<div class="pull-left agregar-imagen"></div>
-	<div class="pull-right visita-menu">Visitas a tu menu <strong>1000</strong></div>
-</div>
-<div class="agrupar"><label>Nombre del Lugar:</label></div>
-<div class="agrupar"><label>Propietario:</label></div>
-<div class="agrupar"><label>Domicilio:</label></div>
-<div class="agrupar"><label>Zona:</label></div>
-<div class="agrupar"><label>Telefono:</label></div>
-<div class="agrupar"><label>Horario:</label></div>
-<div class="agrupar"><label>Pagina:</label></div>
+<div class="seccion">
+	<h1 class="seccionTitulo">Detalles</h1>
+	<hr>
+	<div class="seccionPrincipal">
+		<div class="pull-left agregar-imagen"></div>
+		<div class="pull-right visita-menu">Visitas a tu menu <strong>1000</strong></div>
+	</div>
+	<div class="form-horizontal">
+	  <div class="form-group">
+	    <label for="nombreLugar" class="col-sm-2 control-label">Nombre del Lugar</label>
+	    <div class="col-sm-4">
+	      <input type="text" class="form-control" id="nombreLugar" placeholder="Lugar">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="nombreLugar" class="col-sm-2 control-label">Propietario</label>
+	    <div class="col-sm-4">
+	      <input type="text" class="form-control" id="nombreLugar" placeholder="Lugar">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="nombreLugar" class="col-sm-2 control-label">Domicilio</label>
+	    <div class="col-sm-4">
+	      <input type="text" class="form-control" id="nombreLugar" placeholder="Lugar">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="zona" class="col-sm-2 control-label">Zona</label>
+	    <div class="col-sm-4">
+	      <select class="form-control" id="zona">
+	      	<option value="">Selecciona tu Zona</option>
+	      	<option value="">1</option>
+	      	<option value="">2</option>
+	      	<option value="">3</option>
+	      	<option value="">4</option>
+	      </select>
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="nombreLugar" class="col-sm-2 control-label">Horario</label>
+	    <div class="col-sm-4">
+	      <input type="text" class="form-control" id="nombreLugar" placeholder="Lugar">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="nombreLugar" class="col-sm-2 control-label">Pagina</label>
+	    <div class="col-sm-4">
+	      <input type="text" class="form-control" id="nombreLugar" placeholder="Lugar">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="nombreLugar" class="col-sm-2 control-label">Tipo de Lugar</label>
+	    <div class="col-sm-4">
+		    <label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox1" value="option1"> Restaurante
+			</label>
+			<label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox2" value="option2"> Bar
+			</label>
+			<label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox3" value="option3"> Cafe
+			</label>
+		</div>
+	  </div>
+	  <div class="form-group">
+	    <label for="nombreLugar" class="col-sm-2 control-label">Tipo de Cocina</label>
+	    <div class="col-sm-4">
+		    <label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox1" value="option1"> Mexicana
+			</label>
+			<label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox2" value="option2"> Desayunos
+			</label>
+			<label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox3" value="option3"> Internacional
+			</label>
+		    <label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox1" value="option1"> Argentina
+			</label>
+			<label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox2" value="option2"> Cenaduria
+			</label>
+			<label class="checkbox-inline">
+			  <input type="checkbox" id="inlineCheckbox3" value="option3"> Para llevar
+			</label>
+		</div>
+	  </div>
+	</div><!-- Form -->
+</div><!-- Seccion -->
