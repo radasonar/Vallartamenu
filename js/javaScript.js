3c96e5cbdf0f87f38ebfaa1048238bfd24148fe1
radasonar <rada-99@hotmail.com>
2014-03-15 23:47:46 -0600

    Funcionamiento Parte 1 - Debugueado
    Código limpiado, probado y mejorado.

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,87 +1,98 @@
 $(function() {
-	
-	panel(0);
-	$('form').on('submit', procesarLogin);
-
-	function transicion() {
-			$('#contenido').hide().fadeIn();
-	}
-	function panel(id) {
-			switch(id){
-				case 0:{ $('#contenido').load('php/admin_contenido_detalles.php'); break;}
-				case 1:{ $('#contenido').load('php/admin_contenido_entradas.php');	break;}
-				case 2:{ $('#contenido').load('php/admin_contenido_platillos_fuertes.php'); break;}
-				case 3:{ $('#contenido').load('php/admin_contenido_postres.php');	break;}
-				case 4:{ $('#contenido').load('php/admin_contenido_bebidas.php');	break;}
-				default:{ break;}
-			}
+	$(window).on('load', function() {		
+		panel(0);
+		$('form').on('submit', procesarLogin);
+	});
+});
+function panel(e) {
+	$(function() {
+		switch(e){
+			case 0:{ $('#contenido').load('php/admin_contenido_detalles.php'); break;}
+			case 1:{ $('#contenido').load('php/admin_contenido_entradas.php'); break;}
+			case 2:{ $('#contenido').load('php/admin_contenido_platillos_fuertes.php'); break;}
+			case 3:{ $('#contenido').load('php/admin_contenido_postres.php'); break;}
+			case 4:{ $('#contenido').load('php/admin_contenido_bebidas.php'); break;}
+		}
 		transicion();
-	}
-	function guardarPlatillo(ide){
-			var titulo = $('#titulo').val();
-			var precio = $('#precio').val();
-			var descripcion = $('#descripcion').val();
+	});
+}	
+function guardarPlatillo(e){
+	$(function() {
+		var titulo = $('#titulo').val();
+		var precio = $('#precio').val();
+		var descripcion = $('#descripcion').val();
 
-			if(titulo != false){
-				if(precio != false){
-					if(descripcion != false){
-
-						$.ajax({
-							type: 'POST',
-							url: 'php/insertar_platillo.php',
-							data:{
-								id: ide,
-								tit: titulo,
-								pre: precio,
-								des: descripcion
-								}
-							}).done(function() {
-								cerrarV();
-							});
-					}else{
-						$('#divDescripcion').addClass('has-error').focus();
-					}
-				}else{
-					$('#divPrecio').addClass('has-error').focus();
-				}
-			}else{
-				$('#divTitulo').addClass('has-error').focus();
-			}
-	}
-	function procesarLogin(e) {
-		e.preventDefault();
-			var user = $('input[name=user]').val();
-			var pass = $('input[name=pass]').val();
-
-			if(user != false){
-				$('#divUser').removeClass('has-error');
-				if(pass != false){
-					$('#divPass').removeClass('has-error');
+		if(titulo != false){
+			if(precio != false){
+				if(descripcion != false){
 					$.ajax({
 						type: 'POST',
-						url: 'php/lg_conectar.php',
-						data: {
-							use: user,
-							pas: pass
+						url: 'php/insertar_platillo.php',
+						data:{
+						id: e,
+						tit: titulo,
+						pre: precio,
+						des: descripcion
 						}
 					}).done(function() {
-						alert("Se enviaron correctamente los datos");
-					});	
+							cerrarV();
+					});
 				}else{
-					$('#divPass').addClass('has-error');
+					$('#divDescripcion').addClass('has-error').focus();
 				}
 			}else{
-				$('#divUser').addClass('has-error');
+				$('#divPrecio').addClass('has-error').focus();
 			}
-	}
-	function abrirAgregarPlatillo(ide){
+		}else{
+			$('#divTitulo').addClass('has-error').focus();
+		}
+	});
+}
+function abrirAgregarPlatillo(e){
+	$(function() {
 		abrirV();
-			$('.contenidoVentana').load('php/frm_agregar_platillo.php',{ id: ide});
-	}
-	function abrirV() {
-			$('.ventana').fadeIn();
-	}
-	function cerrarV() {
-			$('.ventana').fadeOut();
-	}
-});
+		$('.contenidoVentana').load('php/frm_agregar_platillo.php',{ id: e});
+	});
+}
+function transicion() {
+	$(function(){
+		$('#contenido').hide().fadeIn();
+	});
+}
+function procesarLogin(e) {
+	$(function(){
+		e.preventDefault();
+		var user = $('input[name=user]').val();
+		var pass = $('input[name=pass]').val();
+		if(user != false){
+			$('#divUser').removeClass('has-error');
+			if(pass != false){
+				$('#divPass').removeClass('has-error');
+				$.ajax({
+					type: 'POST',
+					url: 'php/lg_conectar.php',
+					data: {
+						use: user,
+						pas: pass
+					}
+				}).done(function() {
+					alert("Se enviaron correctamente los datos");
+				});	
+			}else{
+				$('#divPass').addClass('has-error');
+			}
+		}else{
+			$('#divUser').addClass('has-error');
+		}
+	});
+}
+function abrirV() {
+	$(function(){
+		$('.ventana').fadeIn();
+	});
+}
+function cerrarV() {
+	$(function() {
+		$('.ventana').fadeOut();
+	});
+}
